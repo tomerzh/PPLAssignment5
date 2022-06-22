9fedad9b4207b309b71ff619c9f294cb4ecedeba
tomerzh <tomerzkatom7@gmail.com>
2022-06-22 11:26:11 +0300

    racket

---
+++ ex5/ex5.pl
@@ -0,0 +1,16 @@
+/*
+ * **********************************************
+ * Printing result depth
+ *
+ * You can enlarge it, if needed.
+ * **********************************************
+ */
+maximum_printing_depth(100).
+
+:- current_prolog_flag(toplevel_print_options, A),
+   (select(max_depth(_), A, B), ! ; A = B),
+   maximum_printing_depth(MPD),
+   set_prolog_flag(toplevel_print_options, [max_depth(MPD)|B]).
+
+% Signature: unique(List, UniqueList, Dups)/3
+% Purpose: succeeds if and only if UniqueList contains the same elements of List without duplicates (according to their order in List), and Dups contains the duplicates
